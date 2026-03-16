77586a0d3ecbe166e10c103be9cc867dfa8956b6
nahid-info-stack <nahidcse.inf@gmail.com>
2026-03-17 01:59:09 +0600

    added some conditional rendering in country

--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,21 +1,34 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import './country.css';
 
 const Country = ({country}) => {
     //console.log(country)
     const handleVisited = () => {
         //console.log("Button Clicked...")
+        //setVisited(true)
+        // option: 1.Basic if/else
+         if(visited){
+             setVisited(false)
+         }else{
+             setVisited(true)
+         }
+        // option: 2.ternary oparetor
+        //setVisited(visited ? false : true);
+
+        // option: 3.use logical not !
+        //setVisited(!visited)
     }
-   
+   const [visited , setVisited] = useState(false)
     return (
-        <div className='country'>
+        <div className={ ` country  ${visited ? 'country-visited' : 'country-notvisited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Country Name: {country.name.common}</h3>
             <p>Area: {country.area.area}{country.area.area > 300000 ? "Big Countriy" : "Small Countriy"}</p>
             <p>Population: {country.population.population} </p>
             <p>Religion: {country.region.region} </p>
+            <p>Country Code: {country.cca3.cca3} </p>
 
-            <button onClick={handleVisited}> {Visited ? 'Visited' : 'Not Visited'}</button>
+            <button onClick={handleVisited}> {visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };
