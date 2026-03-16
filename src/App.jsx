bec136fb97b3bd082151a83718fb6a389c2aeda0
nahid-info-stack <nahidcse.inf@gmail.com>
2026-03-16 23:14:12 +0600

    react project app set up

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Countrys from './components/countrys'
+
+function App() {
+  
+
+  return (
+    <>
+      
+          <h1>Get started React World</h1>
+          <Countrys name={'Bangladesh'}></Countrys>
+          
+          
+          
+        
+
+      
+
+      
+      
+    </>
+  )
+}
+
+export default App
